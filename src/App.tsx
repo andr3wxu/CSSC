f32bd47ad9d1d90619ac8e53ccae4ccaba2312a5
Andrew Xu <andr3w.xu@gmail.com>
2023-11-09 05:35:03 -0500

    i still hate git

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import TitleCard from "./components/TitleCard.tsx";
 import Navbar from "./components/Navbar.tsx";
 import Page from "./components/Page.tsx";
 import Home from "./pages/Home.tsx";
@@ -8,11 +9,12 @@ import Team from "./pages/Team.tsx";
 import FAQ from "./pages/FAQ.tsx";
 
 const idArray = ["home", "why", "info", "team", "faq"];
-const headingArray = ["Home", "Why?", "Info", "Team", "FAQ"];
+const headingArray = ["Home", "Why", "Info", "Team", "FAQ"];
 
 function App() {
   return (
     <>
+      <TitleCard imgSrc="public/images/cssc-logo.png" />
       <Navbar idArray={idArray} headingArray={headingArray} />
       <Page id="home" content={<Home />} />
       <Page id="why" content={<Why />} />
--- src/components/TitleCard.tsx
+++ src/components/TitleCard.tsx
@@ -1,7 +1,19 @@
 import "../styles/TitleCard.css";
 
-const TitleCard = () => {
-  return <div>TitleCard</div>;
+interface Props {
+  imgSrc: string;
+}
+
+const TitleCard = ({ imgSrc }: Props) => {
+  return (
+    <div className="title-logo-container">
+      <img src={imgSrc} className="title-logo" />
+      <h1 className="title">Calgary Science Spelling Challenge</h1>
+      <h2 className="subtitle">
+        Not your ordinary spelling bee . . . are you ready?
+      </h2>
+    </div>
+  );
 };
 
 export default TitleCard;
