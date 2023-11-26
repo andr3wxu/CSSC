dba5d1ec9bd5e0e5326256ef82675ecb1fd38c94
Andrew Xu <andr3w.xu@gmail.com>
2023-11-26 01:05:46 -0500

    finished info section

--- src/pages/Info.tsx
+++ src/pages/Info.tsx
@@ -33,10 +33,66 @@ const Info = () => {
             <div className="info-box format">
               <div className="info-card">
                 <h1 className="heading sub">FORMAT</h1>
-                <p>
-                  A more detailed schedule will be mailed out closer to the
-                  event date.
-                </p>
+              </div>
+              <div className="blurb info">
+                <div>
+                  <p>
+                    The <span className="keyword">CSSC</span> is split across
+                    three unique rounds:
+                  </p>
+                </div>
+                <div className="round">
+                  <h2 className="round-label">
+                    Round <span className="emphasis info">1</span>
+                  </h2>
+                  <div className="round-info">
+                    <div className="heading sub info">Individual </div>
+                    <p>
+                      <span className="keyword">30 </span>
+                      words from biology, chemistry, or physics worth
+                      <span className="keyword"> 1</span>,{" "}
+                      <span className="keyword">2</span> or{" "}
+                      <span className="keyword">3 </span>
+                      points will be read out, one at a time, to the
+                      competitors. To earn points, the competitor must write
+                      these correctly within a time limit.{" "}
+                      <span className="bold">Most points wins!</span>
+                    </p>
+                  </div>
+                </div>
+                <div className="round">
+                  <h2 className="round-label">
+                    Round <span className="emphasis info">2</span>
+                  </h2>
+                  <div className="round-info">
+                    <div className="heading sub info">Individual </div>
+                    <p>
+                      Competitors will first have to correctly verbally spell as
+                      many words as possible in{" "}
+                      <span className="keyword">2</span> time-pressed speed
+                      rounds. Top performers then advance to a traditional
+                      spelling bee type knockout stage where they will take
+                      turns verbally spelling words.{" "}
+                      <span className="bold">
+                        The last person standing wins!
+                      </span>
+                    </p>
+                  </div>
+                </div>
+                <div className="round">
+                  <h2 className="round-label">
+                    Round <span className="emphasis info">3</span>
+                  </h2>
+                  <div className="round-info">
+                    <div className="heading sub info">Team </div>
+                    <p>
+                      After <span className="keyword">3</span> preliminary
+                      cycles, the top <span className="keyword">8</span> teams
+                      will go head-to-head in a bracket to determine the winner
+                      of the <span className="bold">CSSC trophy</span>.
+                    </p>
+                  </div>
+                </div>
               </div>
             </div>
             <div className="photo-container">
@@ -64,6 +120,10 @@ const Info = () => {
                 />
               </div>
             </div>
+          </div>
+          <div className="medal-trophy">
+            <img src="/images/medals.png" className="image info medal" />
+            <img src="/images/trophy.png" className="image info trophy" />
           </div>
         </div>
       </div>
